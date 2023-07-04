ec0a1b3891b3f23d621f9a0d3b3791adcee3b2d6
nam.nghiem <nam.nghiem@savvycomsoftware.com>
2023-07-04 23:52:08 +0700

    Update ✨: State of Component 🐱‍🚀

--- src/views/Example/Welcome.js
+++ src/views/Example/Welcome.js
@@ -1,8 +1,32 @@
 import React from "react";
 
 class Welcome extends React.Component {
+
+    state = {
+        name: "Nam Nghiem",
+        channel: "Anime Offical"
+    }
+
+    handleOnChangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
     render(){
-        return <h1> Welcome to My React Project </h1>
+        return (
+            <>
+            <div className="first">
+                <input value={this.state.name} type="text" 
+                    onChange={(event) => this.handleOnChangeName(event)}/>
+                <br></br>
+                <h1>My name is : {this.state.name} </h1>
+            </div>
+            <div className="last">
+                My youtube channel : {this.state.channel}
+            </div>
+            </>
+        );
     }
 }
 
